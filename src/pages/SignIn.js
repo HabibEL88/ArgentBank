456cf3919d524f3ce52a5865f9ecbfafd6578a1c
HabibEL88 <elmouini.habib@gmail.com>
2023-02-04 00:16:14 +0100

    create signin page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,32 @@
 import React from "react";
+import "../style/SignIn.css";
 
-const signIn = () => {
-  return <main className="Signin"></main>;
+const SignIn = () => {
+  return (
+    <React.Fragment>
+      <main class="main bg-dark">
+        <section class="sign-in-content">
+          <i class="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          <form>
+            <div class="input-wrapper">
+              <label for="username">Username</label>
+              <input type="text" id="username" />
+            </div>
+            <div class="input-wrapper">
+              <label for="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <div class="input-remember">
+              <input type="checkbox" id="remember-me" />
+              <label for="remember-me">Remember me</label>
+            </div>
+            <button class="sign-in-button">Sign In</button>
+          </form>
+        </section>
+      </main>
+    </React.Fragment>
+  );
 };
 
-export default signIn;
+export default SignIn;
