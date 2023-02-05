d66ff341b9800899a73a02aa630bb482d4557bef
HabibEl88 <elmouini.habib@gmail.com>
2023-02-05 21:57:50 +0100

    header logout feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,47 @@ import { Link } from "react-router-dom";
 import Logo from "../assets/argentBankLogo.png";
 import "../style/Header.css";
 
+import { useSelector, useDispatch } from "react-redux";
+import { setLogout } from "features/user/userSlice";
+import { resetState } from "features/browser/browserStorage";
+
 const Header = () => {
+
+  const dispatch = useDispatch();
+
+  const loginInfos = useSelector(state => state.user.loginInfos)
+
+  const userName = useSelector(state => state.user.firstName)
+
+  const SignOut = (userName) => {
+    return (
+    <div className="signHeader">   
+      <Link to="/profile" className="main-nav-item">
+        <i class="fa fa-user-circle"></i>
+        {userName}
+      </Link>
+      <Link to="/"className="main-nav-item" onClick={() => {
+        dispatch(setLogout())
+        resetState()
+        }}>
+        <i className="fa fa-sign-out"></i>
+        Sign Out
+      </Link>
+    </div>
+    )
+  }
+
+  const SignIn = () => {
+    return (
+      <div>
+        <Link to="/SignIn" className="main-nav-item">
+          <i className="fa fa-user-circle"></i>
+          Sign In
+        </Link>
+      </div>
+    )
+  }
+
   return (
     <header>
       <nav className="main-nav">
@@ -15,10 +55,12 @@ const Header = () => {
           />
         </Link>
         <div>
-          <Link to="/SignIn" className="main-nav-item">
-            <i className="fa fa-user-circle"></i>
-            Sign In
-          </Link>
+          {loginInfos ? 
+          SignOut(userName)
+          : 
+          SignIn()
+          }
+          
         </div>
       </nav>
     </header>
